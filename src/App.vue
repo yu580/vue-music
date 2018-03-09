5483c85d69f92e7962e51c3c59b9836af02fa101
yu580 <929248550@qq.com>
2018-03-09 14:24:06 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <header></header>
+  </div>
+</template>
+
+<script>
+import Header from './components/header/header'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="m-header">
+      <div class="icom"></div>
+      <h1 class="text">Chicken music</h1>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="stylus" scoped>
+    @import '~common/stylus/mixin'
+    @import '~common/stylus/variable'
+    
+    .m-header
+        position relative
+        height 44px
+        color $color-theme
+        font-size 0
+        .icon
+            display inline-block
+            vertical-align top 
+            margin-top 6px
+            width 30px
+            height 32px
+            bg-image('logo')
+            background-size 30px 32px
+        .text
+            display inline-block
+            vertical-align top 
+            font-size $font-size-large
+
+
+</style>
+
+
